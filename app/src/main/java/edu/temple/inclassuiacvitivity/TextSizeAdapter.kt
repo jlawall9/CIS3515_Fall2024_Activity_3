6f1a6e8db8c95b9412851bf4bed25d615c7bc817
John Lawall <tun19502@temple.edu>
2024-09-19 18:51:50 -0400

    finished

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -18,10 +18,19 @@ class TextSizeAdapter(private val context: Context, private val numbers: Array<I
     override fun getItemId(position: Int) = position.toLong()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val textView = TextView(context)
-        textView.text = numbers[position].toString()
-        textView.textSize = 22f
-        textView.setPadding(5,10,0,10)
+
+        val textView : TextView
+
+        if (convertView != null) {
+            textView = TextView(context)
+        } else{
+            textView = TextView(context)
+            textView.text = numbers[position].toString()
+
+            textView.setPadding(5,10,0,10)
+        }
+        textView.textSize = numbers[position].toFloat()
+
         return textView
 
     }
